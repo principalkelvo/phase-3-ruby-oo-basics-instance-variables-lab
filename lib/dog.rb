b823da797af4cb3bfe54753c9a6644f3a9a6bc60
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-23 10:40:20 +0300

    getter and setters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-
-end
+     # setter method
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+      # getter method
+    def name
+      @this_dogs_name
+    end
+  end
